372c8451c2f062362fe403c86e8a6ad301419ce6
SB-9838 <86655602+SB-9838@users.noreply.github.com>
2021-08-26 01:06:41 +0300

    Delete Tools directory

--- Tools/colors.py
+++
@@ -1,6 +0,0 @@
-from colorama import Fore
-class colors:
-	cyan = Fore.CYAN
-	green = Fore.GREEN
-	red = Fore.RED
-	w = Fore.WHITE
--- Tools/csv2json.py
+++
@@ -1,9 +0,0 @@
-from csv2json import convert, load_csv, save_json
-from Tools.colors import colors as c
-def csv2json(path, output):
-	with open(path) as r,open(output, 'w') as w:
-		try:
-			convert(r, w)
-			print(f"{c.green}[TOOL] Succesfully converted {path} to {output}{c.w}!")
-		except Exception as e:
-			print(f"{c.red}[TOOL] An error has occured!\nERROR:\n{e}{c.w}")
